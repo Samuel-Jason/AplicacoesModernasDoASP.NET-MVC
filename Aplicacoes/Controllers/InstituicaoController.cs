043c8882e194e4f78881415cdfafde0b5eaaf6b2
Samuel <samuelljason@gmail.com>
2023-10-03 22:03:57 -0300

    Aplicacoes modernas do ASP.NET

---
+++ Aplicacoes/Controllers/InstituicaoController.cs
@@ -0,0 +1,47 @@
+﻿using Aplicacoes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacoes.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoId = 1,
+                Nome = "UniParana",
+                Endereco = "Parana"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 2,
+                Nome = "Unisanta",
+                Endereco = "santa catarina"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 3,
+                Nome = "UniSaoPaulo",
+                Endereco = "SaoPaulo"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 4,
+                Nome = "UniSulgrandence",
+                Endereco = "Rio grande do sul"
+            },
+            new Instituicao()
+            {
+                InstituicaoId = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de janeiro"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+    }
+}
---
+++ Aplicacoes/Models/Instituicao.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplicacoes.Models
+{
+    public class Instituicao
+    {
+        public long? InstituicaoId { get; set; } //MANTEM A IDENTIDADE DE CADA OBJETO
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+    }
+}
